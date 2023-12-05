ea78aed68e401efd0a55e6355f1dd1757dcdb7ff
vnzchr <152945651+vnzchr@users.noreply.github.com>
2023-12-05 14:40:32 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,11 +1,11 @@
 const wrapper = document.querySelector(".wrapper");
 const question = document.querySelector(".question");
 const gif = document.querySelector(".gif");
-const yesBtn = document.querySelector(".yesBtn");
-const noBtn =document.querySelector(".noBtn");
+const yesBtn = document.querySelector(".yes-btn");
+const noBtn = document.querySelector(".no-btn");
 
 yesBtn.addEventListener("click", () => {
-  question.innerhtml = "aaaa, i like u too as a friend lol";
+  question.innerHTML = "Aaaaa, I like you too";
   gif.src =
     "https://raw.githubusercontent.com/DzarelDeveloper/Img/main/gif.webp";
 });
